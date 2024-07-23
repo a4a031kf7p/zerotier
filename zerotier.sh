fed0758ccc4fa14243abe5f965b4394d318f8e4d
a4a031kf7p <a4a031kf7p@gmail.com>
2024-07-22 23:40:21 -0300

    I made a few changes to the encrypted token, tweaking it a bit and adjusting the options here and there. I switched it over to the $HOME variable instead of ~, and so on. Version 1.2

--- zerotier.sh
+++ zerotier.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 
-#VARIABLES +++++
-error="Error, try --help or -h for help."
-#token=~/.z3rotier/.... TO DO 
 
-#Message for help use +++++
+#Save token encrypted
+save_token(){ #TODO
+token="$(xclip -o -selection clipboard)"
+echo "$token" > token.temp
+token_name="$2"
+gpg -c $HOME/.zerotier/token.temp
+mv $HOME/.zerotier/token.temp.gpg "${token_name}.gpg"
+gpg -q -d "$HOME/.zerotier/${token_name}.gpg"
+shred -zu $HOME/.zerotier/token.temp
+}
+if [ "$1" = "--save-token" ] || [ "$1" = "-st" ]; then #TODO
+save_token
+exit 20
+else
+echo $error
+exit 020
+fi
+
+
+#Message for help use
 help(){
-echo "z3rotier 1.1
-Usage: z3rotier [Options] {token} {network} {member}
-              --create-token TO DO
-              --list-network TOKEN 
+echo "zerotier 1.2
+Usage: zerotier [Options] {token} {network} {member}
+              --save-token NAME TODO
+              --list-network TOKEN
               --list-member TOKEN NETWORK
               --authorize TOKEN NETWORK MEMBER
               --deauthorize TOKEN NETWORK MEMBER
 Options Summary
-  -ct, --create-token: you'll be asked for a name for your token. use the name of the token instead of the token itself. TO DO
+  -st, --save-token: just save the token encrypted and use the name of the token instead of the token itself. You don't have to paste your token into the terminal to keep your account secure. make sure it's in your clipboard for the first time you create it. TO DO
   -ln, --list-network: list current networks.
   -lm, --list-members: list network members.
   -a, --authorize: authorize a network member.
@@ -29,7 +45,7 @@ exit 021
 fi
 
 
-#List current network +++++
+#List current network
 list_current_network(){
 curl -s -H "Authorization: token $2" "https://api.zerotier.com/api/v1/network" | jq '.[] | [
     .id,
@@ -39,7 +55,7 @@ curl -s -H "Authorization: token $2" "https://api.zerotier.com/api/v1/network" |
     .config.creationTime,
     .config.ipAssignmentPools[0].ipRangeStart,
     .config.ipAssignmentPools[0].ipRangeEnd
-  ]' | jq -rs '.[] | @csv' 2>> ~/.z3rotier/error.log
+  ]' | jq -rs '.[] | @csv' 2>> $HOME/.zerotier/error.log
 }
 if [ "$1" = "--list-network" ] || [ "$1" = "-ln" ]; then
 list_current_network
@@ -50,7 +66,7 @@ exit 022
 fi
 
 
-#List network members +++++
+#List network members
 list_network_members(){
 curl -s -H "Authorization: token $2" "https://api.zerotier.com/api/v1/network/$3/member" | jq '.[] | [
     .id,
@@ -58,7 +74,7 @@ curl -s -H "Authorization: token $2" "https://api.zerotier.com/api/v1/network/$3
     .physicalAddress,
     .ipAssignments,
     .name
-  ]' | jq -rs '.[] | @csv' 2>> ~/.z3rotier/error.log
+  ]' | jq -rs '.[] | @csv' 2>> $HOME/.zerotier/error.log
 }
 if [ "$1" = "--list-members" ] || [ "$1" = "-lm" ]; then
 list_network_members
@@ -69,11 +85,10 @@ exit 023
 fi
 
 
-#Authorize a network member +++++
+#Authorize a network member
 authorize(){
-curl -s -H "Authorization: token $2" -X POST "https://api.zerotier.com/api/v1/network/$3/member/$4" --data '{"config": {"authorized": true}}' 2>> ~/.z3rotier/error.log
+curl -s -H "Authorization: token $2" -X POST "https://api.zerotier.com/api/v1/network/$3/member/$4" --data '{"config": {"authorized": true}}' 2>> $HOME/.zerotier/error.log
 }
-
 if [ "$1" = "--authorize" ] || [ "$1" = "-a" ]; then
 authorize
 exit 24
@@ -85,14 +100,16 @@ fi
 
 #Deauthorize a network member
 deauthorize(){
-curl -s -H "Authorization: token $2" -X POST "https://api.zerotier.com/api/v1/network/$3/member/$4" --data '{"config": {"authorized": false}}' 2>> ~/.z3rotier/error.log
+curl -s -H "Authorization: token $2" -X POST "https://api.zerotier.com/api/v1/network/$3/member/$4" --data '{"config": {"authorized": false}}' 2>> $HOME/.zerotier/error.log
 }
 if ["$1" = "--deauthorize" ] || [ "$1" = "-d" ]; then
 deauthorize
 exit 25
 else
-#Message for when there are no compatible arguments
 echo $error
 exit 025
 fi
 
+
+#Message for when there are no compatible arguments
+error="Error, try --help or -h for help."
