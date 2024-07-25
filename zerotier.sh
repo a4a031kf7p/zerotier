32b8683a39287471f4c74ac8e6af215dd923c83d
a4a031kf7p <a4a031kf7p@gmail.com>
2024-07-25 01:27:18 -0300

    Save token feature. Fixed program exit on else statements. Updated the --help section.
    Version 1.3

--- zerotier.sh
+++ zerotier.sh
@@ -1,53 +1,67 @@
 #!/bin/bash
 
 
+#Message for when there are no compatible arguments
+error="Error, try --help or -h for help."
+
+
+#Checking files
+if [ -f "$HOME/.zerotier/errors.log" ]; then
+      echo -n ""
+else
+      mkdir -p "$HOME/.zerotier"
+fi
+
+
+#Input capture
+argument_1=$1
+argument_2=$2
+argument_3=$3
+argument_4=$4
+token_name=$(grep "$argument_2" "$HOME/.zerotier/zerotier.conf" | tail -n1)
+
+
+#Testing argument_2
+if [ "$token_name" = "$argument_2" ] 2>> "$HOME/.zerotier/errors.log"; then
+     token=$(gpg -q -d "$HOME/.zerotier/${argument_2}.gpg") 2>> "$HOME/.zerotier/errors.log"
+     authorization="Authorization: token "$token""
+else
+     authorization="Authorization: token "$argument_2""
+fi
+
+
 #Save token encrypted
-save_token(){ #TODO
+save_token(){ 
 token="$(xclip -o -selection clipboard)"
-echo "$token" > token.temp
-token_name="$2"
+echo "$token" > $HOME/.zerotier/token.temp
+echo "$argument_2" >> "$HOME/.zerotier/zerotier.conf"
 gpg -c $HOME/.zerotier/token.temp
-mv $HOME/.zerotier/token.temp.gpg "${token_name}.gpg"
-gpg -q -d "$HOME/.zerotier/${token_name}.gpg"
+mv $HOME/.zerotier/token.temp.gpg "$HOME/.zerotier/${argument_2}.gpg"
 shred -zu $HOME/.zerotier/token.temp
 }
-if [ "$1" = "--save-token" ] || [ "$1" = "-st" ]; then #TODO
-save_token
-exit 20
-else
-echo $error
-exit 020
-fi
 
 
 #Message for help use
 help(){
-echo "zerotier 1.2
+echo "zerotier 1.3
 Usage: zerotier [Options] {token} {network} {member}
-              --save-token NAME TODO
-              --list-network TOKEN
-              --list-member TOKEN NETWORK
-              --authorize TOKEN NETWORK MEMBER
-              --deauthorize TOKEN NETWORK MEMBER
+    --save-token NAME 
+    --list-network TOKEN
+    --list-member TOKEN NETWORK
+    --authorize TOKEN NETWORK MEMBER
+    --deauthorize TOKEN NETWORK MEMBER
 Options Summary
-  -st, --save-token: just save the token encrypted and use the name of the token instead of the token itself. You don't have to paste your token into the terminal to keep your account secure. make sure it's in your clipboard for the first time you create it. TO DO
-  -ln, --list-network: list current networks.
-  -lm, --list-members: list network members.
-  -a, --authorize: authorize a network member.
-  -d, --deauthorize: deauthorize a network member." 
+    -st, --save-token: Encrypt and save your token from the clipboard.
+    -ln, --list-network: List current networks.
+    -lm, --list-members: List network members.
+    -a, --authorize: Authorize a network member.
+    -d, --deauthorize: Deauthorize a network member." 
 }
-if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
-help
-exit 21
-else
-echo $error
-exit 021
-fi
 
 
 #List current network
 list_current_network(){
-curl -s -H "Authorization: token $2" "https://api.zerotier.com/api/v1/network" | jq '.[] | [
+curl -s -H "$authorization" "https://api.zerotier.com/api/v1/network" | jq '.[] | [
     .id,
     .config.name,
     .config.description,
@@ -55,61 +69,58 @@ curl -s -H "Authorization: token $2" "https://api.zerotier.com/api/v1/network" |
     .config.creationTime,
     .config.ipAssignmentPools[0].ipRangeStart,
     .config.ipAssignmentPools[0].ipRangeEnd
-  ]' | jq -rs '.[] | @csv' 2>> $HOME/.zerotier/error.log
+  ]' | jq -rs '.[] | @csv' 2>> $HOME/.zerotier/errors.log
 }
-if [ "$1" = "--list-network" ] || [ "$1" = "-ln" ]; then
-list_current_network
-exit 22
-else
-echo $error
-exit 022
-fi
 
 
 #List network members
 list_network_members(){
-curl -s -H "Authorization: token $2" "https://api.zerotier.com/api/v1/network/$3/member" | jq '.[] | [
+curl -s -H "$authorization" "https://api.zerotier.com/api/v1/network/$argument_3/member" | jq '.[] | [
     .id,
     .lastOnline,
     .physicalAddress,
     .ipAssignments,
     .name
-  ]' | jq -rs '.[] | @csv' 2>> $HOME/.zerotier/error.log
+  ]' | jq -rs '.[] | @csv' 2>> $HOME/.zerotier/errors.log
 }
-if [ "$1" = "--list-members" ] || [ "$1" = "-lm" ]; then
-list_network_members
-exit 23
-else
-echo $error
-exit 023
-fi
 
 
 #Authorize a network member
 authorize(){
-curl -s -H "Authorization: token $2" -X POST "https://api.zerotier.com/api/v1/network/$3/member/$4" --data '{"config": {"authorized": true}}' 2>> $HOME/.zerotier/error.log
+curl -s -H "$authorization" -X POST "https://api.zerotier.com/api/v1/network/$argument_3/member/$argument_4" --data '{"config": {"authorized": true}}' | grep -Po "\"authorized\"\:\S{1,4}" 2>> $HOME/.zerotier/errors.log
 }
-if [ "$1" = "--authorize" ] || [ "$1" = "-a" ]; then
-authorize
-exit 24
-else
-echo $error
-exit 024
-fi
 
 
 #Deauthorize a network member
 deauthorize(){
-curl -s -H "Authorization: token $2" -X POST "https://api.zerotier.com/api/v1/network/$3/member/$4" --data '{"config": {"authorized": false}}' 2>> $HOME/.zerotier/error.log
+curl -s -H "$authorization" -X POST "https://api.zerotier.com/api/v1/network/$argument_3/member/$argument_4" --data '{"config": {"authorized": false}}' | grep -Po "\"authorized\"\:\S{1,5}" 2>> $HOME/.zerotier/errors.log
 }
-if ["$1" = "--deauthorize" ] || [ "$1" = "-d" ]; then
-deauthorize
+
+
+if [ "$1" = "--save-token" ] || [ "$1" = "-st" ]; then
+    save_token
+exit 20
+fi
+if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
+    help
+exit 21
+fi
+if [ "$1" = "--list-network" ] || [ "$1" = "-ln" ]; then
+    list_current_network
+exit 22
+fi
+if [ "$1" = "--list-members" ] || [ "$1" = "-lm" ]; then
+    list_network_members
+exit 23
+fi
+if [ "$1" = "--authorize" ] || [ "$1" = "-a" ]; then
+    authorize
+exit 24
+fi
+if [ "$1" = "--deauthorize" ] || [ "$1" = "-d" ]; then
+    deauthorize 
 exit 25
 else
-echo $error
-exit 025
+    echo $error
+    exit 26
 fi
-
-
-#Message for when there are no compatible arguments
-error="Error, try --help or -h for help."
